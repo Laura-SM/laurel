4298cede6f9177f1d01e5c7ec9c746341e6d7b6f
Laura <laura.sabater.musach@gmail.com>
2021-06-01 12:08:25 +0200

    Modify backend index

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,14 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const authRoutes = require('./routes/auth.routes');
+const usersRoutes = require('./routes/users.routes');
+const plantsRoutes = require('./routes/plants.routes');
+
 require('dotenv').config();
+require('./passport/passport.config');
+require('./ddbb/mongoose.config');
 
 const server = express();
 const port = process.env.PORT || 2021;
@@ -18,12 +25,17 @@ mongoose.connect(
 );
 
 server.use(cors());
-server.use(express.json());
 server.use(morgan('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
-const plantsRouter = require('./routes/plants.routes');
-
-server.use('/plants', plantsRouter);
+server.use('/', authRoutes);
+server.use(
+  '/user',
+  passport.authenticate('jwt', { session: false }),
+  usersRoutes,
+);
+server.use('/plants', plantsRoutes);
 
 server.listen(port,
   () => debug(`Server is running in ${chalk.blue(`localhost:${port}`)}`));
