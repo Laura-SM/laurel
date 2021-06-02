aa585d97fc8560d73cb693e1d5885ef97fb7e148
Laura <laura.sabater.musach@gmail.com>
2021-06-02 16:44:58 +0200

    Create plantsReducer

---
+++ frontend/src/redux/reducers/plantsReducer.js
@@ -0,0 +1,26 @@
+import actionTypes from '../actions/actionTypes';
+
+function plantsReducer(plants = [], action) {
+  let updatedPlants = [];
+  switch (action.type) {
+    case actionTypes.LOAD_PLANTS:
+      updatedPlants = action.plants;
+      break;
+    case actionTypes.ADD_PLANT:
+      updatedPlants = [...plants, action.plant];
+      break;
+    case actionTypes.DELETE_PLANT:
+      updatedPlants = plants.filter(plant => plant._id !== action.plantId);
+      break;
+    case actionTypes.UPDATE_PLANT:
+      updatedPlants = plants.map(plant =>
+        plant._id === action.plant.id ? {...plant, ...action.plant} : plant,
+      );
+      break;
+    default:
+      return updatedPlants;
+  }
+  return updatedPlants;
+}
+
+export default plantsReducer;
