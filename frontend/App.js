31396c61e8ac550d025015e0cec2c0061e85d7ed
Laura <laura.sabater.musach@gmail.com>
2021-06-02 16:46:14 +0200

    Add Provider in App

--- frontend/App.js
+++ frontend/App.js
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {Provider} from 'react-redux';
+import configureStore from './src/redux/store/index';
 import Login from './src/components/login/login';
 import Home from './src/components/home/home';
 
@@ -9,16 +11,18 @@ const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{title: 'Login'}}
-        />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={configureStore()}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{title: 'Login'}}
+          />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
