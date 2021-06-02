319f3cb7030df1bca652e0782cf8b24888616d76
Laura <laura.sabater.musach@gmail.com>
2021-06-02 16:46:31 +0200

    Modify home component

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,12 +1,32 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import {PropTypes} from 'prop-types';
+import {connect} from 'react-redux';
+import {loadPlants} from '../../redux/actions/actionCreators';
 import styles from './home.styes';
 
-const Home = ({navigation}) => {
+const Home = ({navigation, plants, dispatch}) => {
+  useEffect(() => {
+    if (!plants.length) {
+      dispatch(loadPlants());
+    }
+  }, []);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <View className="plants-menu">
+        {plants.length ? (
+          <Text>Tenim plantes</Text>
+        ) : (
+          <Text> No tenim plantes</Text>
+        )}
+        {plants.map(plant => (
+          <Text>{plant.name}</Text>
+        ))}
+      </View>
       <TouchableOpacity
         style={styles.button}
         title="Go back"
@@ -16,4 +36,20 @@ const Home = ({navigation}) => {
   );
 };
 
-export default Home;
+Home.propTypes = {
+  plants: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(store) {
+  return {
+    plants: store.plants,
+  };
+}
+
+export default connect(mapStateToProps)(Home);
