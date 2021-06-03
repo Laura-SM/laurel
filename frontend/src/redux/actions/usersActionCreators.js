6ee749ac1e705b30ae55f9e71236e8106517c222
Laura <laura.sabater.musach@gmail.com>
2021-06-03 15:36:13 +0200

    Create userActionsCreators

---
+++ frontend/src/redux/actions/usersActionCreators.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import actionTypes from './actionTypes';
+
+const url = 'http://192.168.8.104:2021/login';
+
+export function loadUser(user) {
+  return async dispatch => {
+    try {
+      const {data} = await axios.post(url, user);
+      dispatch({
+        type: actionTypes.GET_USER,
+        user: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.USER_ERROR,
+      });
+    }
+  };
+}
