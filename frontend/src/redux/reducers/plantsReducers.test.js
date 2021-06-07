c847195f8a2e926a1f7ad84ef29c937217b762bc
Laura <laura.sabater.musach@gmail.com>
2021-06-07 15:01:49 +0200

    Create plantsReducers test

---
+++ frontend/src/redux/reducers/plantsReducers.test.js
@@ -0,0 +1,63 @@
+import plantsReducer from './plantsReducer';
+import actionTypes from '../actions/actionTypes';
+
+describe('Given plantsReducer function ', () => {
+  test('when resolved with LOAD_PLANTS, then should return updatedPlants', () => {
+    expect(
+      plantsReducer([], {
+        type: actionTypes.LOAD_PLANTS,
+        plants: [{name: 'kentia'}],
+      }),
+    ).toEqual([{name: 'kentia'}]);
+  });
+
+  test('when resolved with ADD_PLANT, then should return updatedPlants', () => {
+    expect(
+      plantsReducer([{name: 'kentia'}], {
+        type: actionTypes.ADD_PLANT,
+        plant: {name: 'potus'},
+      }),
+    ).toEqual([{name: 'kentia'}, {name: 'potus'}]);
+  });
+
+  test('when resolved with DELETE_PLANT, then should return updatedPlants', () => {
+    expect(
+      plantsReducer(
+        [
+          {name: 'kentia', _id: 1},
+          {name: 'potus', _id: 2},
+        ],
+        {
+          type: actionTypes.DELETE_PLANT,
+          plantId: 2,
+        },
+      ),
+    ).toEqual([{name: 'kentia', _id: 1}]);
+  });
+
+  test('when resolved with UPDATE_PLANT, then should return updatedPlants', () => {
+    expect(
+      plantsReducer(
+        [
+          {name: 'kentia', _id: 1},
+          {name: 'potus', _id: 2},
+        ],
+        {
+          type: actionTypes.UPDATE_PLANT,
+          plant: {name: 'sanseviera', _id: 2},
+        },
+      ),
+    ).toEqual([
+      {name: 'kentia', _id: 1},
+      {name: 'sanseviera', _id: 2},
+    ]);
+  });
+
+  test('when resolved with default, then should return updatedPlants', () => {
+    expect(
+      plantsReducer([{name: 'kentia', _id: 1}], {
+        type: actionTypes.PLANTS_ERROR,
+      }),
+    ).toEqual([]);
+  });
+});
