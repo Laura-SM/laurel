483b54bc96081c4f8dfc36621bf6f767d1ab56c3
Laura <laura.sabater.musach@gmail.com>
2021-06-09 12:31:52 +0200

    Create MyRemindersComponent

---
+++ frontend/src/components/MyReminders/MyReminders.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const MyReminders = () => {
+  return (
+    <View>
+      <Text>My Reminders</Text>
+    </View>
+  );
+};
+
+export default MyReminders;
