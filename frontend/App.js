e4e327cf5cca991240465309fc8e4b03c17656c0
Laura <laura.sabater.musach@gmail.com>
2021-06-09 16:07:00 +0200

    Delete stackNavigator and create unic bottomTabNavigator

--- frontend/App.js
+++ frontend/App.js
@@ -1,34 +1,62 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import {Provider} from 'react-redux';
 import configureStore from './src/redux/store/index';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import SignIn from './src/components/SignIn/SignIn';
 import SignUp from './src/components/SignUp/SignUp';
-import BottomNavigator from './src/components/BottomNavigator/BottomNavigator';
-
 import PlantDetails from './src/components/PlantDetails/PlantDetails';
 import CardPlant from './src/components/CardPlant/CardPlant';
 import MyPlantDetails from './src/components/MyPlantDetails/MyPlantDetails';
 import CardMyPlant from './src/components/CardMyPlant/CardMyPlant';
+import MyReminders from './src/components/MyReminders/MyReminders';
+import PlantsSearcher from './src/components/PlantsSearcher/PlantsSearcher';
+import MyPlants from './src/components/MyPlants/MyPlants';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <Provider store={configureStore()}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="SignIn">
-          <Stack.Screen name="BottomNavigator" component={BottomNavigator} />
-          <Stack.Screen name="SignIn" component={SignIn} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-
-          <Stack.Screen name="PlantDetails" component={PlantDetails} />
-          <Stack.Screen name="CardPlant" component={CardPlant} />
-          <Stack.Screen name="MyPlantDetails" component={MyPlantDetails} />
-          <Stack.Screen name="CardMyPlant" component={CardMyPlant} />
-        </Stack.Navigator>
+        <Tab.Navigator
+          initialRouteName="SignIn"
+          screenOptions={({route}) => ({
+            tabBarButton: [
+              'SignIn',
+              'SignUp',
+              'PlantDetails',
+              'CardPlant',
+              'MyPlantDetails',
+              'CardMyPlant',
+            ].includes(route.name)
+              ? () => {
+                  return null;
+                }
+              : undefined,
+          })}
+          tabBarOptions={{
+            showLabel: true,
+          }}>
+          <Tab.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{tabBarVisible: false}}
+          />
+          <Tab.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{tabBarVisible: false}}
+          />
+          <Tab.Screen name="MyReminders" component={MyReminders} />
+          <Tab.Screen name="PlantsSearcher" component={PlantsSearcher} />
+          <Tab.Screen name="MyPlants" component={MyPlants} />
+          <Tab.Screen name="PlantDetails" component={PlantDetails} />
+          <Tab.Screen name="CardPlant" component={CardPlant} />
+          <Tab.Screen name="MyPlantDetails" component={MyPlantDetails} />
+          <Tab.Screen name="CardMyPlant" component={CardMyPlant} />
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
