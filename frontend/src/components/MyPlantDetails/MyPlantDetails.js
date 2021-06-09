0c7257f9cc91bec02b0d03e268ea155e49f8ae0f
Laura <laura.sabater.musach@gmail.com>
2021-06-09 12:32:37 +0200

    Create MyPlantDetails component

---
+++ frontend/src/components/MyPlantDetails/MyPlantDetails.js
@@ -0,0 +1,55 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect} from 'react';
+import {Text, View, Image, ScrollView, TouchableOpacity} from 'react-native';
+import {connect} from 'react-redux';
+import {loadPlant} from '../../redux/actions/plantsActionCreators';
+import styles from '../PlantDetails/PlantDetails.styles';
+import globalStyles from '../../styles/global.styles';
+
+function MyPlantDetails({
+  selectedPlant,
+  dispatch,
+  route,
+  navigation: {goBack},
+}) {
+  const {plantId} = route.params;
+
+  useEffect(() => {
+    if (!selectedPlant.name) {
+      dispatch(loadPlant(plantId));
+    }
+  }, [plantId]);
+
+  return (
+    <ScrollView style={globalStyles.mainContainer}>
+      <Text>Vull que funcioni</Text>
+      <Text style={globalStyles.titleText}>{selectedPlant.name}</Text>
+      <Text style={globalStyles.subTitleText}>
+        {selectedPlant.scientificName}
+      </Text>
+      <View style={styles.featuresContainer}>
+        <Image style={styles.image} source={{uri: selectedPlant.image}} />
+        <View>
+          <Text>{selectedPlant.waterNeeds}</Text>
+          <Text>{selectedPlant.mistNeeds}</Text>
+          <Text>{selectedPlant.lightNeeds}</Text>
+          <Text>{selectedPlant.petFriendly}</Text>
+        </View>
+      </View>
+      <Text style={globalStyles.text}>{selectedPlant.info}</Text>
+      <TouchableOpacity
+        style={globalStyles.roundButton}
+        onPress={() => goBack()}>
+        <Text>Back</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+}
+
+function mapStateToProps({selectedPlant}) {
+  return {
+    selectedPlant,
+  };
+}
+
+export default connect(mapStateToProps)(MyPlantDetails);
