3bdabe8cf78eeb741d55cfd5d70f5a984f9bcee4
Laura <laura.sabater.musach@gmail.com>
2021-06-09 12:29:28 +0200

    Dispatch loadPlants after signIn

--- frontend/src/components/signIn/signIn.js
+++ frontend/src/components/signIn/signIn.js
@@ -9,11 +9,12 @@ import {
 } from 'react-native';
 import {connect} from 'react-redux';
 import {signInUser} from '../../redux/actions/authActionCreators';
+import {loadPlants} from '../../redux/actions/plantsActionCreators';
 import styles from './SignIn.styles';
 
 const SignIn = ({dispatch, userAccess, navigation}) => {
   useEffect(() => {
-    userAccess.token && navigation.navigate('Search');
+    userAccess.token && navigation.navigate('BottomNavigator');
   }, [userAccess, navigation]);
 
   let [email, setEmailInputValue] = useState('');
@@ -22,6 +23,10 @@ const SignIn = ({dispatch, userAccess, navigation}) => {
   const onPressSignIn = () => {
     dispatch(signInUser({email, password}));
   };
+
+  if (userAccess.token) {
+    dispatch(loadPlants());
+  }
 
   return (
     <View style={styles.mainView}>
