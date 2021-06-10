e949877cbc3f1c922161529da4aa0c243253d56a
Laura <laura.sabater.musach@gmail.com>
2021-06-10 20:13:06 +0200

    Create usersReducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import plantsReducer from './plantsReducer';
 import selectedPlantReducer from './selectedPlantReducer';
 import signInReducer from './signInReducer';
 import signUpReducer from './signUpReducer';
+import usersReducer from './usersReducer';
 
 const rootReducer = combineReducers({
   plants: plantsReducer,
   selectedPlant: selectedPlantReducer,
   userAccess: signInReducer,
   user: signUpReducer,
+  users: usersReducer,
 });
 
 export default rootReducer;
