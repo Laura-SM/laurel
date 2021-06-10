c5c3903bc3f5d8a1ac1b084bd0b808727127cad1
Laura <laura.sabater.musach@gmail.com>
2021-06-10 20:14:42 +0200

    Modify CardMyPlant

--- frontend/src/components/CardMyPlant/CardMyPlant.js
+++ frontend/src/components/CardMyPlant/CardMyPlant.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Image, Pressable} from 'react-native';
+import {Image, Pressable, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import styles from './CardMyPlant.styles';
 
 const CardMyPlant = ({plant}) => {
   const navigation = useNavigation();
   return (
-    <Pressable
-      onPress={() =>
-        navigation.navigate('MyPlantDetails', {plantId: plant._id})
-      }>
-      <Image style={styles.image} source={{uri: plant.image}} />
-    </Pressable>
+    <>
+      <Text>{plant.name}</Text>
+      <Pressable
+        onPress={() =>
+          navigation.navigate('MyPlantDetails', {plantId: plant._id})
+        }>
+        <Image style={styles.image} source={{uri: plant.image}} />
+      </Pressable>
+    </>
   );
 };
 
