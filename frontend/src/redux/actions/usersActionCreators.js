e9f1c5317160f36805baddcce1145528a9d2ca6e
Laura <laura.sabater.musach@gmail.com>
2021-06-10 20:14:07 +0200

    Create usersActionCreator and add actionTypes

---
+++ frontend/src/redux/actions/usersActionCreators.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import actionTypes from './actionTypes';
+
+const url = 'http://192.168.0.57:2021/users';
+
+export function updateUser(user) {
+  return async dispatch => {
+    try {
+      const {data} = await axios.put(`${url}/${user._id}`, user);
+      dispatch({
+        type: actionTypes.UPDATE_USER,
+        user: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.USER_ERROR,
+      });
+    }
+  };
+}
