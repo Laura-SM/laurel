262a404e8108347dff7f4a8069946201a26813d8
Laura <laura.sabater.musach@gmail.com>
2021-06-15 14:03:58 +0200

    Change switch for if/else at signUpReducer

--- frontend/src/redux/reducers/signUpReducer.js
+++ frontend/src/redux/reducers/signUpReducer.js
@@ -1,14 +1,11 @@
 import actionTypes from '../actions/actionTypes';
 
 function signUpReducer(user = {}, action) {
-  let updatedUser = user;
-  switch (action.type) {
-    case actionTypes.SIGNUP_USER:
-      updatedUser = action.user;
-      break;
-    default:
-      updatedUser;
-      break;
+  let updatedUser = {};
+  if (action.type === actionTypes.SIGNUP_USER) {
+    updatedUser = action.user;
+  } else {
+    updatedUser = user;
   }
   return updatedUser;
 }
