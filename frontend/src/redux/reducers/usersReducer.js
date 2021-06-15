da428003b9d81460f72d4f77b19e3288f3dd3733
Laura <laura.sabater.musach@gmail.com>
2021-06-15 14:03:38 +0200

    Change switch for if/else at usersReducer

--- frontend/src/redux/reducers/usersReducer.js
+++ frontend/src/redux/reducers/usersReducer.js
@@ -1,16 +1,13 @@
 import actionTypes from '../actions/actionTypes';
 
 function usersReducer(users = [], action) {
-  let updatedUsers = users;
-  switch (action.type) {
-    case actionTypes.UPDATE_USER:
-      updatedUsers = users.map(user =>
-        user._id === action.user._id ? {...user, ...action.user} : user,
-      );
-      break;
-    default:
-      updatedUsers;
-      break;
+  let updatedUsers = [];
+  if (action.type === actionTypes.UPDATE_USER) {
+    updatedUsers = users.map(user =>
+      user._id === action.user._id ? {...user, ...action.user} : user,
+    );
+  } else {
+    updatedUsers = users;
   }
   return updatedUsers;
 }
